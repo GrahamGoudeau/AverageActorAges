7d662a9a144aeaba1137e8ae274adde131facfc9
Graham Goudeau <grahamgoudeau@gmail.com>
2015-09-03 09:55:58 -0400

    Constructing url-safe movie title searches

--- average_actors_age.py
+++ average_actors_age.py
@@ -2,8 +2,19 @@ import urllib
 import requests
 import json
 
-r = requests.get('http://www.myapifilms.com/imdb/inTheaters?actors=S')
-content = json.loads(r.content)
+currently_playing_api = 'http://www.myapifilms.com/imdb/inTheaters?actors=S'
+title_search_api = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'
+
+if False:
+    r = requests.get(currently_playing_api)
+    with open('content.txt', 'w') as f:
+        f.write(r.content)
+
+    content = json.loads(r.content)
+else:
+    print "Not making api call"
+    with open('content.txt', 'r') as f:
+        content = json.loads(f.read())
 
 in_theaters = []
 for entry in content:
@@ -16,5 +27,12 @@ for entry in content:
 movie_actor_map = {}
 actor_age_map = {}
 
-    #print movie['movies']['originalTitle']
-    #print movie
+# requires a url-safe movie title
+def get_actor_list(title):
+    url = title_search_api.format(title)
+    print url
+
+for movie in in_theaters:
+    # encode movie titles as utf8 and make them url-safe
+    url_safe_movie = urllib.quote_plus(movie.encode('utf8'))
+    get_actor_list(url_safe_movie)
