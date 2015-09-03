fe9f8936be40927869ab3e6305e975aee71d7878
Graham Goudeau <grahamgoudeau@gmail.com>
2015-09-03 13:18:28 -0400

    Successfully parsing birth years from actors wiki pages

--- average_actors_age.py
+++ average_actors_age.py
@@ -1,3 +1,4 @@
+import re
 import urllib
 import requests
 import json
@@ -7,6 +8,8 @@ currently_playing_api = 'http://www.myapifilms.com/imdb/inTheaters?actors=S'
 # format the api urls with the movie title or actor and send a GET request
 title_search_api = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'
 actor_search_api = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles={}&rvsection=0'
+
+birth_date_regex = r'\| birth_date\s*=\s*{{.*?\|(\d+).*}}'
 
 def get_current_movies():
     if False:
@@ -41,7 +44,6 @@ def get_url_safe_actor(string):
 
 # requires a url-safe movie title
 def get_actor_list(title):
-    url = title_search_api.format(title)
     r = requests.get(title_search_api.format(title))
     content = json.loads(r.content)
     actors_string = content['Actors']
@@ -51,6 +53,22 @@ def get_actor_list(title):
             actors_list[index] = actor[1:]
 
     return actors_list
+
+# requires url-safe actor name
+def get_actor_age(actor):
+    r = requests.get(actor_search_api.format(actor))
+    content = json.loads(r.content)
+    pages = content['query']['pages']
+
+    # expect only one page
+    page_number = pages.keys()[0]
+    if 'missing' in pages[page_number]:
+        # actor not on wikipedia or name different
+        return None
+    infobox = pages[page_number]['revisions'][0]['*']
+    search_obj = re.search(birth_date_regex, infobox)
+    if search_obj:
+        return int(search_obj.group(1))
 
 '''
 movie_actor_map = {}
@@ -75,4 +93,6 @@ if __name__ == "__main__":
         ages = []
         for actor in actor_list:
             #age = get_actor_age(get_url_safe_actor(actor))
-            get_url_safe_actor(actor)
+            ages.append(get_actor_age(get_url_safe_actor(actor)))
+        print title
+        print ages
