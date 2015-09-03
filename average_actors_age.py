60ad3b3447c8eaeb763a188ee8e507cd5bb332b1
Graham Goudeau <grahamgoudeau@gmail.com>
2015-09-03 14:11:36 -0400

    Computing average ages for some movies

--- average_actors_age.py
+++ average_actors_age.py
@@ -3,10 +3,13 @@ import urllib
 import requests
 import json
 
+current_year = 2015
+
 currently_playing_api = 'http://www.myapifilms.com/imdb/inTheaters?actors=S'
 
 # format the api urls with the movie title or actor and send a GET request
-title_search_api = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'
+title_search_api = 'http://www.omdbapi.com/?t={}&plot=short&r=json&y=' + str(current_year)
+print title_search_api
 actor_search_api = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles={}&rvsection=0'
 
 birth_date_regex = r'\| birth_date\s*=\s*{{.*?\|(\d+).*}}'
@@ -46,6 +49,8 @@ def get_url_safe_actor(string):
 def get_actor_list(title):
     r = requests.get(title_search_api.format(title))
     content = json.loads(r.content)
+    if 'Error' in content:
+        return []
     actors_string = content['Actors']
     actors_list = actors_string.split(',')
     for index, actor in enumerate(actors_list):
@@ -68,7 +73,7 @@ def get_actor_age(actor):
     infobox = pages[page_number]['revisions'][0]['*']
     search_obj = re.search(birth_date_regex, infobox)
     if search_obj:
-        return int(search_obj.group(1))
+        return current_year - int(search_obj.group(1))
 
 '''
 movie_actor_map = {}
@@ -84,7 +89,11 @@ print r.content
 
 if __name__ == "__main__":
     currently_playing_titles = get_current_movies()
+    movie_age_map = {}
+
+    # cache actor names we have seen already
     actor_age_map = {}
+
     for title in currently_playing_titles:
         safe_title = get_url_safe_title(title)
         actor_list = get_actor_list(safe_title)
@@ -92,7 +101,20 @@ if __name__ == "__main__":
 
         ages = []
         for actor in actor_list:
-            #age = get_actor_age(get_url_safe_actor(actor))
-            ages.append(get_actor_age(get_url_safe_actor(actor)))
-        print title
-        print ages
+            safe_actor = get_url_safe_actor(actor)
+
+            # if seen this actor already, use the cached value
+            if safe_actor in actor_age_map:
+                ages.append(actor_age_map[safe_actor])
+            else:
+                age = get_actor_age(safe_actor)
+
+                # only consider ages we actually found
+                if age is not None:
+                    ages.append(age)
+                    actor_age_map[safe_actor] = age
+
+        if len(ages) > 0:
+            print title
+            print "Average age:"
+            print (sum(ages) * 1.0) / len(ages)
